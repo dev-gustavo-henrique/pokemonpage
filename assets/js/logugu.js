720e4b0e07725426f42caeda5961cc874c0a2ad0
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-01-28 19:18:37 -0300

    regatoring the code in logugu.js

--- assets/js/logugu.js
+++ assets/js/logugu.js
@@ -1,58 +1,77 @@
-let userLogado = JSON.parse(localStorage.getItem("userLogado"))
-let logado = document.querySelector("#logado")
-let nomeCima = document.querySelector("#cad-log")
-let estrelasCima = document.querySelector("#cad-estrelas")
-let estrelasCimaValor = JSON.parse(localStorage.getItem("Estrelas"))
+//***  ---  Data Initial  ---  ***
+let userLogado = JSON.parse(localStorage.getItem("userLogado"));
+let logado = document.querySelector("#logado");
 
-listaUser = JSON.parse(localStorage.getItem("listaUser"))
+let nomeCima = document.querySelector("#cad-log");
+let estrelasCima = document.querySelector("#cad-estrelas");
+let estrelasCimaValor = JSON.parse(localStorage.getItem("Estrelas"));
+
+listaUser = JSON.parse(localStorage.getItem("listaUser"));
 
 let {
     Estrelas
-} = JSON.parse(localStorage.getItem("userLogado"))
+} = JSON.parse(localStorage.getItem("userLogado"));
 
+nomeCima.innerHTML = userLogado.nome;
 
-nomeCima.innerHTML = userLogado.nome
+estrelasCima.innerHTML = userLogado.Estrelas + "★";
 
-estrelasCima.innerHTML = userLogado.Estrelas + "★"
-
-
+///*** 
 if (localStorage.getItem("token") == null) {
-    alert("Você precisa esta logado!")
-    window.location.href = "./login.html"
+    alert("Você precisa esta logado!");
+
+    window.location.href = "./login.html";
 }
 
+//***  ---  Functions  ---  ***
+
+//**** function sair
 function sair() {
-    encontrarUsuario()
-    localStorage.removeItem("token")
-    localStorage.removeItem("userLogado")
-    window.location.href = "./login.html"
+    ///***  
+    encontrarUsuario();
+
+    ///***  
+    localStorage.removeItem("token");
+    localStorage.removeItem("userLogado");
+
+    ///***  
+    window.location.href = "./login.html";
 }
+//**** -----------
 
+
+//**** function encontrar usuario
 function encontrarUsuario() {
-
     for (var i = 0; i < listaUser.length; i++) {
+        ///***  
         let [{
             valorInicial
-        }] = JSON.parse(localStorage.getItem("listaUser"))
-        let lista = listaUser[i]
-        let valorcheio = lista.valorInicial
-        let achei = userLogado.nome === lista.nomeCad
-        console.log(achei, valorcheio, lista)
+        }] = JSON.parse(localStorage.getItem("listaUser"));
 
+        let lista = listaUser[i];
+        let valorcheio = lista.valorInicial;
+        let achei = userLogado.nome === lista.nomeCad;
+
+        ///***  
+        console.log(achei, valorcheio, lista);
+
+        ///***  
         if (userLogado.nome === lista.nomeCad) {
+            ///***  
+            valorcheio = lista.valorInicial = Estrelas;
 
+            ///***  
+            const NewStarUser = [...listaUser];
+            localStorage.setItem("listaUser", JSON.stringify(NewStarUser));
 
-            let valorcheio = lista.valorInicial = Estrelas
+            ///***  
+            console.log(NewStarUser);
 
-            const NewStarUser = [...listaUser]
-
-            localStorage.setItem("listaUser", JSON.stringify(NewStarUser))
-
-            console.log(NewStarUser)
-
-            return
+            ///***  
+            return;
         } else {
-            console.log("false", valorInicial)
+            console.log("false", valorInicial);
         }
     }
 }
+//**** -----------
