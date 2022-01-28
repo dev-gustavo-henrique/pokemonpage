b94803d16ba39a169673628c4645cabe16cfc25b
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-01-28 19:17:57 -0300

    regatoring the code in estrelas.js

--- assets/js/estrelas.js
+++ assets/js/estrelas.js
@@ -1,12 +1,18 @@
+//***  ---  Data Initial  ---  ***
 let userLogado = JSON.parse(localStorage.getItem("userLogado"));
 let logado = document.querySelector("#logado");
+
 let nomeCima = document.querySelector("#cad-log");
 let estrelasCima = document.querySelector("#cad-estrelas");
+
 let deposito = document.getElementsByClassName("deposito");
 let compra = document.getElementsByClassName("compra");
+
+let btnAdicionar = document.getElementsByClassName("adicionar")[0];
+let btnReduzir = document.getElementsByClassName("reduzir")[0];
+
 let { Estrelas } = JSON.parse(localStorage.getItem("userLogado"));
 let { user } = JSON.parse(localStorage.getItem("userLogado"));
-
 let listaUser = JSON.parse(localStorage.getItem("listaUser"));
 
 //console.log(valorInicial)
@@ -14,114 +20,172 @@ let listaUser = JSON.parse(localStorage.getItem("listaUser"));
 //let valornovo = (listaUser[index].valorInicial = Estrelas)
 //const NewStarUser = {...ListaUser, valorInicial:valornovo}
 
-document
-  .getElementsByClassName("adicionar")[0]
-  .addEventListener("click", encontrarUsuario());
-document
-  .getElementsByClassName("reduzir")[0]
-  .addEventListener("click", encontrarUsuario());
+//***  ---  Events Main  ---  ***
+btnAdicionar.addEventListener("click", encontrarUsuario());
 
+btnReduzir.addEventListener("click", encontrarUsuario());
+
+
+//***  ---  Functions  ---  ***
+
+//**** function encontrar usuario
 function encontrarUsuario() {
+  ///***  ---
   for (var i = 0; i < listaUser.length; i++) {
+    ///***  
     let [{ valorInicial }] = JSON.parse(localStorage.getItem("listaUser"));
     let lista = listaUser[i];
-    let valorcheio = lista.valorInicial;
+    let valorCheio = lista.valorInicial;
     let achei = userLogado.nome === lista.nomeCad;
-    console.log(achei, valorcheio, lista);
+
+    ///*** 
+    console.log(achei, valorCheio, lista);
 
     if (userLogado.nome === lista.nomeCad) {
-      let valorcheio = (lista.valorInicial = Estrelas);
+      ///***  
+      valorCheio = (lista.valorInicial = Estrelas);
 
+      ///***  
       const NewStarUser = [...listaUser];
 
+      ///***  
       localStorage.setItem("listaUser", JSON.stringify(NewStarUser));
-
       console.log(NewStarUser);
 
       return;
+
     } else {
+      ///*** 
       console.log("false", valorInicial);
     }
   }
 }
+//**** -----------
 
-nomeCima.innerHTML = userLogado.nome;
 
-estrelasCima.innerHTML = userLogado.Estrelas + "★";
-
-if (localStorage.getItem("token") == null) {
-  alert("Você precisa esta logado!");
-  window.location.href = "./login.html";
-}
-
+//**** function sair
 function sair() {
+  ///***  ---
   encontrarUsuario();
 
+  ///***  ---
   localStorage.removeItem("token");
   localStorage.removeItem("userLogado");
 
+  ///***  ---
+  window.location.href = "./login.html";
+}
+//**** -----------
+
+
+//**** function capturar soma
+function capturarSoma() {
+  ///***  ---
+  let { Estrelas } = JSON.parse(localStorage.getItem("userLogado"));
+  capturarValor = document.getElementById("deposito").value;
+  somaTotal = parseInt(capturarValor) + Estrelas;
+
+  ///***  ---
+  if (somaTotal < Estrelas) {
+    //***
+    alert("Não é possivel usar valores negativos!");
+
+  } else if (capturarValor == "") {
+    //***
+    alert("insira um valor!");
+
+  } else {
+    //***
+    const NewStarUser = {
+      ...userLogado,
+      Estrelas: somaTotal
+    };
+
+    //***
+    localStorage.setItem("userLogado", JSON.stringify(NewStarUser));
+
+    //***
+    estrelasCima.innerHTML = NewStarUser + "★";
+    console.log(NewStarUser);
+
+    window.location.href = "./estrelas.html";
+  }
+
+  //***
+  encontrarUsuario();
+}
+//**** -----------
+
+
+//**** function capturar soma
+function capturarSaque() {
+  ///***  ---
+  let { Estrelas } = JSON.parse(localStorage.getItem("userLogado"));
+  capturarValor = document.getElementById("saque").value;
+  somaTotal = Estrelas - parseInt(capturarValor);
+
+  ///***  ---
+  if (capturarValor === "") {
+    //***
+    alert("insira um valor!");
+
+  } else if (capturarValor <= 0) {
+    //***
+    alert("Não é possivel usar valores negativos!");
+
+  } else if (somaTotal < 0) {
+    //***
+    alert("Saldo insuficiente!");
+
+    return;
+
+  } else {
+    //***
+    const NewStarUser = {
+      ...userLogado,
+      Estrelas: somaTotal
+    };
+
+    //***
+    localStorage.setItem("userLogado", JSON.stringify(NewStarUser));
+    estrelasCima.innerHTML = NewStarUser + "★";
+
+    //***
+    console.log(NewStarUser);
+
+    window.location.href = "./estrelas.html";
+  }
+
+  //***
+  encontrarUsuario();
+}
+//**** -----------
+
+
+//**** function inut value
+function inputValue(event) {
+  ///***  ---
+  var input = event.target;
+}
+//**** -----------
+
+
+//***  ---  Work in starts and user  ---  ***
+
+///***  ---
+nomeCima.innerHTML = userLogado.nome;
+estrelasCima.innerHTML = userLogado.Estrelas + "★";
+
+///***  ---
+if (localStorage.getItem("token") === null) {
+  //***  
+  alert("Você precisa estar logado!");
   window.location.href = "./login.html";
 }
 
 //document.getElementsByClassName("adicionar")[0].addEventListener("click", inputValue)
 
+///***  ---
 var capturarValor = "";
 
-function capturarSoma() {
-  let { Estrelas } = JSON.parse(localStorage.getItem("userLogado"));
-  capturarValor = document.getElementById("deposito").value;
-  somaTotal = parseInt(capturarValor) + Estrelas;
 
-  if (somaTotal < Estrelas) {
-    alert("Não é possivel usar valores negativos!");
-  } else if (capturarValor == "") {
-    alert("insira um valor!");
-  } else {
-    const NewStarUser = {
-      ...userLogado,
-      Estrelas: somaTotal
-    };
-
-    localStorage.setItem("userLogado", JSON.stringify(NewStarUser));
-
-    estrelasCima.innerHTML = NewStarUser + "★";
-
-    console.log(NewStarUser);
-
-    window.location.href = "./estrelas.html";
-  }
-  encontrarUsuario();
-}
-
-function capturarSaque() {
-  let { Estrelas } = JSON.parse(localStorage.getItem("userLogado"));
-  capturarValor = document.getElementById("saque").value;
-  somaTotal = Estrelas - parseInt(capturarValor);
-
-  if (capturarValor == "") {
-    alert("insira um valor!");
-  } else if (capturarValor <= 0) {
-    alert("Não é possivel usar valores negativos!");
-  } else if (somaTotal < 0) {
-    alert("Saldo insuficiente!");
-    return;
-  } else {
-    const NewStarUser = {
-      ...userLogado,
-      Estrelas: somaTotal
-    };
-
-    localStorage.setItem("userLogado", JSON.stringify(NewStarUser));
-
-    estrelasCima.innerHTML = NewStarUser + "★";
-
-    console.log(NewStarUser);
-
-    window.location.href = "./estrelas.html";
-  }
-  encontrarUsuario();
-}
-
-function inputValue(event) {
-  var input = event.target;
-}
